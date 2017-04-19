37aed1d35a4fb56b62e1a7da797270ea4cc1a50d
fernandodamasceno <fernando_damasceno@hotmail.com>
2017-04-18 22:22:36 -0300

    Update TabelaProduto.sql

--- TabelaProduto.sql
+++ TabelaProduto.sql
@@ -1,3 +1,4 @@
+DROP TABLE PRODUTOS;
 CREATE TABLE PRODUTOS
 (
 NOME VARCHAR(5000) NOT NULL,
@@ -10,37 +11,35 @@ PRIMARY KEY (SKU)
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
 VALUES (
-"MXIII Android Google TV Player w/ 2GB RAM, 8GB ROM, Wi-Fi - Black",
-"Form Color: Black Built-in Memory / RAM: 2GB Storage: 8GB Power Adapter: EU Plug Model: MXIII Quantity: 1 piece Material: Plastic Shade Of Color: Black Operating",
+'MXIII Android Google TV Player w/ 2GB RAM, 8GB ROM, Wi-Fi - Black',
+'Form Color: Black Built-in Memory / RAM: 2GB Storage: 8GB Power Adapter: EU Plug Model: MXIII Quantity: 1 piece Material: Plastic Shade Of Color: Black Operating',
   82, 336696, 181.32
 );
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
 VALUES (
-"MK809IV Quad-Core Android Google TV Player w/2GB RAM, 8GB ROM",
-"The powerful RK3188 adopts 28nm technology, uses Quad-core Cortex-A9 and Mali-400 MP4 GPU, has a CPU clock speed of up to 1.6Ghz. Its 2G RAM and Android4.2 system",
+'MK809IV Quad-Core Android Google TV Player w/2GB RAM, 8GB ROM',
+'The powerful RK3188 adopts 28nm technology, uses Quad-core Cortex-A9 and Mali-400 MP4 GPU, has a CPU clock speed of up to 1.6Ghz. Its 2G RAM and Android4.2 system',
   116, 217396, 129.66
 );
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
 VALUES (
-"M8S Amlogic S812 Quad-core TV Box w/2GB RAM, 8GB ROM - Black (EU Plug)",
-"Form Color: Black Built-in Memory / RAM: 2GB Storage: 8GB Power Adapter: EU Plug Model: M8S Quantity: 1 piece Material: Plastic Shade Of Color: Black Operating System: ",
+'M8S Amlogic S812 Quad-core TV Box w/2GB RAM, 8GB ROM - Black (EU Plug)',
+'Form Color: Black Built-in Memory / RAM: 2GB Storage: 8GB Power Adapter: EU Plug Model: M8S Quantity: 1 piece Material: Plastic Shade Of Color: Black Operating System: ',
   5, 413805, 161.11
 );
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
 VALUES (
-"GULEEK i8S WIN10 / Android Super PC w/ 32GB ROM, HDMI,US Plugs - Silver",
-"Form Color: Silver Built-in Memory / RAM: 2GB Storage: 32GB Power Adapter: US Plugs Model: i8S Quantity: 1 set Material: Aluminum alloy Shade Of Color: Silver Operating",
+'GULEEK i8S WIN10 / Android Super PC w/ 32GB ROM, HDMI,US Plugs - Silver',
+'Form Color: Silver Built-in Memory / RAM: 2GB Storage: 32GB Power Adapter: US Plugs Model: i8S Quantity: 1 set Material: Aluminum alloy Shade Of Color: Silver Operating',
   10, 418763, 265.37
 );
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
 VALUES (
-"Mirascreen 2.4G Wireless HDMI Wi-Fi Display Dongle AirPlay DLNA Miracast",
-"Quantity: 1 piece Form Color: Black Material: ABS Shade Of Color: Black Chipset: AM8252 Output Resolution: 1080P Menu Language: English , Greek , Danish&nb",
+'Mirascreen 2.4G Wireless HDMI Wi-Fi Display Dongle AirPlay DLNA Miracast',
+'Quantity: 1 piece Form Color: Black Material: ABS Shade Of Color: Black Chipset: AM8252 Output Resolution: 1080P Menu Language: English , Greek , Danish&nb',
   13, 414384, 44.60
 );
-
-
