1183179b684f0907d562c4c84dc6bf235740de44
fernandodamasceno <fernando_damasceno@hotmail.com>
2017-04-18 22:02:13 -0300

    Update TabelaProduto.sql

--- TabelaProduto.sql
+++ TabelaProduto.sql
@@ -9,4 +9,7 @@ PRIMARY KEY (SKU)
 );
 
 INSERT INTO PRODUTOS(NOME, DESCRICAO, AVALIACAO, SKU, PRECO)
-VALUES ()
+VALUES (
+"MXIII Android Google TV Player w/ 2GB RAM, 8GB ROM, Wi-Fi - Black",
+"Form Color: Black Built-in Memory / RAM: 2GB Storage: 8GB Power Adapter: EU Plug Model: MXIII Quantity: 1 piece Material: Plastic Shade Of Color: Black Operating",
+)
