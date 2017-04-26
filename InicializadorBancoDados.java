ab10b8db006c3ade3abce6233c8848de150098ea
Ludgero Mascarenhas <ludgeromascarenhas@users.noreply.github.com>
2017-04-25 22:19:33 -0300

    Add files via upload

---
+++ InicializadorBancoDados.java
@@ -0,0 +1,43 @@
+
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import org.apache.derby.jdbc.EmbeddedDataSource;
+
+public class InicializadorBancoDadosDataSource {
+
+    public static String DB_NAME = "cadastro";
+    public static String USER_NAME = "usuario";
+    public static String PASSWORD = "senha";
+    private static DataSource dataSource;
+
+    private static DataSource criarDataSource() throws Exception {
+        if (dataSource == null) {
+            EmbeddedDataSource ds = new EmbeddedDataSource();
+            ds.setDatabaseName(DB_NAME);
+            ds.setCreateDatabase("create");
+            ds.setUser(USER_NAME);
+            ds.setPassword(PASSWORD);
+            dataSource = ds;
+        }
+        return dataSource;
+    }
+
+    public static void criarBd() throws Exception {
+        try (Connection con = criarDataSource().getConnection();
+                Statement sta = con.createStatement()) {
+            String sqlProduto = "CREATE TABLE PRODUTOS("
+                    + "SKU int PRIMARY KEY NOT NULL,"
+                    + "NOME varchar(5000) NOT NULL,"
+                    + "DESCRICAO varchar(5000) NOT NULL,"
+                    + "AVALIACAO float NOT NULL,"
+                    + "PRECO float NOT NULL)";
+            sta.executeUpdate(sqlProduto);
+            
+        }
+    }
+
+    public static Connection conectarBd() throws Exception {
+        return criarDataSource().getConnection();
+    }
+}
